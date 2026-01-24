33bf96a4196c631d66b7213fbab10fa9f1baf023
Lluc Simó <lluc.simo@protonmail.com>
2026-01-24 10:31:30 +0100

    timeshift: remove header from build.rs
    Part-of: https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2832

--- generic/timeshift/build.rs
+++ generic/timeshift/build.rs
@@ -1,18 +1,3 @@
-// Copyright (C) 2026 Lluc Simó Margalef <lsimmar@upv.es>, Immersive
-//    Interactive Media (IIM) R&D group at Universitat Politècnica de València.
-//
-// This plugin has been developed with support by the following projects:
-// CIAICO/2022/025, from Conselleria de Innovación, Universidades, Ciencia y
-// Sociedad Digital of the GVA (DOGV 8919/05.10.2020); and grant
-// PID2021-126645OB-I00, funded by MICIU/AEI/10.13039/501100011033/ and by "ERDF
-// A way of making Europe".
-//
-// This Source Code Form is subject to the terms of the Mozilla Public License, v2.0.
-// If a copy of the MPL was not distributed with this file, You can obtain one at
-// <https://mozilla.org/MPL/2.0/>.
-//
-// SPDX-License-Identifier: MPL-2.0
-
 fn main() {
     gst_plugin_version_helper::info()
 }
