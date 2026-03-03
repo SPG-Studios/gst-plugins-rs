fe5494c24301994959c933d9040b3bb5bc53e2f9
Amyspark <amy@centricular.com>
2026-03-03 13:12:57 -0300

    video/image: remove format as a documented API flag
    This is driven via negotiation now.

--- video/image/src/encoder/mod.rs
+++ video/image/src/encoder/mod.rs
@@ -6,16 +6,11 @@ use gst::prelude::*;
 
 mod imp;
 
-use crate::utils::Format;
-
 glib::wrapper! {
     pub struct Encoder(ObjectSubclass<imp::Encoder>) @extends gst_video::VideoEncoder, gst::Element, gst::Object;
 }
 
 pub fn register(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
-    #[cfg(feature = "doc")]
-    Format::static_type().mark_as_plugin_api(gst::PluginAPIFlags::empty());
-
     gst::Element::register(
         Some(plugin),
         "imagersenc",
