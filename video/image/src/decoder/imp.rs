0467d51e71ab30147bfa73a763704d70e08bf732
Amyspark <amy@centricular.com>
2026-03-03 11:18:59 -0300

    video/image: negotiate caps like a VideoDecoder

--- video/image/src/decoder/imp.rs
+++ video/image/src/decoder/imp.rs
@@ -536,25 +536,69 @@ impl ImageRsDecoder {
         }
     }
 
-    fn get_capslist(&self, filter: Option<&gst::CapsRef>) -> gst::Caps {
-        let mut capslist = gst::Caps::new_empty();
-        {
-            let capslist = capslist.get_mut().unwrap();
-            for mime in mimetypes() {
-                capslist.append_structure(gst::Structure::new_empty(mime));
+    /// __gst_video_element_proxy_getcaps:
+    /// 
+    /// @element: a #GstElement
+    /// 
+    /// @sinkpad: the element's sink #GstPad
+    /// 
+    /// @srcpad: the element's source #GstPad
+    /// 
+    /// @initial_caps: initial caps
+    /// 
+    /// @filter: filter caps
+    /// 
+    /// Returns caps that express @initial_caps (or sink template caps if
+    /// @initial_caps == NULL) restricted to resolution/format/...
+    /// combinations supported by downstream elements (e.g. muxers).
+    /// 
+    /// Returns: a #GstCaps owned by caller
+    fn proxy_get_caps(&self, initial_caps: Option<&gst::CapsRef>, filter: Option<&gst::CapsRef>) -> gst::Caps {
+        /* Allow downstream to specify width/height/framerate/PAR constraints
+        * and forward them upstream for video converters to handle
+        */
+        let templ_caps = initial_caps.map(|v| v.copy()).unwrap_or_else(|| {
+            self.sinkpad.pad_template_caps()
+        });
+        let src_templ_caps = self.srcpad.pad_template_caps();
+        let peer_caps = if let Some(filter) = filter && !filter.is_any() {
+            let proxy_filter = self.proxy_get_caps(Some(&src_templ_caps), Some(filter));
+            self.srcpad.peer_query_caps(Some(&proxy_filter))
+        } else {
+            self.srcpad.peer_query_caps(None)
+        };
+
+        let allowed = peer_caps.intersect_with_mode(&src_templ_caps,
+            gst::CapsIntersectMode::First);
+
+        drop (src_templ_caps);
+        drop (peer_caps);
+
+        let fcaps = if allowed.is_any() {
+            templ_caps
+        } else if allowed.is_empty() {
+            allowed
+        } else {
+            gst::log!(CAT, imp = self, "template caps {}", templ_caps);
+            gst::log!(CAT, imp = self, "allowed caps {}", allowed);
+    
+            let filter_caps = self.proxy_get_caps(Some(&templ_caps), Some(&allowed));
+    
+            let mut fcaps = filter_caps.intersect(&templ_caps);
+            drop (filter_caps);
+            drop (templ_caps);
+    
+            if let Some(f) = filter {
+                gst::log!(CAT, imp = self, "intersecting with {}", f);
+                fcaps = fcaps.intersect(f);
             }
-        }
 
-        let tmpl_caps = ImageRsDecoder::pad_templates()[1].caps();
-        let mut return_caps = capslist.intersect(tmpl_caps);
+            fcaps
+        };
 
-        if let Some(f) = filter
-            && !return_caps.is_empty()
-        {
-            return_caps = return_caps.intersect(f);
-        }
+        gst::log!(CAT, imp = self, "proxy caps {}", fcaps);
 
-        return_caps
+        fcaps
     }
 
     fn sink_query(&self, pad: &gst::Pad, query: &mut gst::QueryRef) -> bool {
@@ -563,7 +607,8 @@ impl ImageRsDecoder {
         match query.view_mut() {
             QueryViewMut::Caps(q) => {
                 let filter = q.filter();
-                let caps = self.get_capslist(filter);
+                // See gst_video_decoder_sink_getcaps
+                let caps = self.proxy_get_caps(None, filter);
                 q.set_result(&caps);
                 true
             }
